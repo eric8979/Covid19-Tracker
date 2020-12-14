5d36f210d64871c5d378ee564b79faac55c106e2
eric8979 <buckelwal8979@khu.ac.kr>
2020-12-14 14:28:01 +0900

    Fix menubar

--- App.js
+++ App.js
@@ -5,7 +5,52 @@ const statistics = new Statistics();
 const countries = new Countries();
 const history = new History();
 
-document.getElementById('menuBtn').addEventListener('click', menuToggle);
+$('.ui.sidebar')
+	.sidebar({
+		context: $('.bottom.segment')
+	})
+	.sidebar('attach events', '.menu .item');
+
+document.querySelector('#homeBtn').addEventListener('click', click);
+document.querySelector('#statsBtn').addEventListener('click', click);
+document.querySelector('#historyBtn').addEventListener('click', click);
+document.querySelector('#globeBtn').addEventListener('click', click);
+const homePage = document.querySelector('#home');
+const historyPage = document.querySelector('#history');
+const statsPage = document.querySelector('#stats');
+const globePage = document.querySelector('#globe');
+
+function click(e) {
+	if (e.target.id === 'homeBtn') {
+		console.log(e.target);
+		homePage.style.display = 'block';
+		historyPage.style.display = 'none';
+		statsPage.style.display = 'none';
+		globePage.style.display = 'none';
+	} else if (e.target.id === 'historyBtn') {
+		homePage.style.display = 'none';
+		historyPage.style.display = 'block';
+		statsPage.style.display = 'none';
+		globePage.style.display = 'none';
+	} else if (e.target.id === 'statsBtn') {
+		homePage.style.display = 'none';
+		historyPage.style.display = 'none';
+		statsPage.style.display = 'block';
+		globePage.style.display = 'none';
+	} else if (e.target.id === 'globeBtn') {
+		homePage.style.display = 'none';
+		historyPage.style.display = 'none';
+		statsPage.style.display = 'none';
+		globePage.style.display = 'flex';
+		globePage.style.justifyContent = 'center';
+		globePage.style.alignItems = 'center';
+		globePage.style.flexDirection = 'column';
+		globePage.style.height = '43rem';
+	}
+}
+
+//
+// ---------------------------------
 
 statistics
 	.getStatistics()
@@ -21,7 +66,3 @@ history
 	.getHistory()
 	.then(data => console.log(data))
 	.catch(err => console.log(err));
-
-function menuToggle() {
-	$('.ui.labeled.icon.sidebar').sidebar('toggle');
-}
